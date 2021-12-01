aa8891f704f305d862dc8470480c4a23c51c5fa3
geniusman420 <71469467+geniusman420@users.noreply.github.com>
2021-12-01 22:38:13 +0000

    Adding files

--- run - TEST.bat
+++ run - TEST.bat
@@ -1,25 +1,25 @@
 @echo off
+setlocal EnableDelayedExpansion
 title Iris - Terminal
 set title=[94mIris - Terminal[0m
-echo %title%
+echo !title!
 echo.
 echo Pushing to GitHub? (Y/N)
 set /p pushyn=
 cls
-echo %title%
+echo !title!
 echo.
-if "%pushyn%"=="Y" (
+if "!pushyn!"=="Y" (
     git add .
     echo Input Commit Message:
     set /p commit_message=
-    echo %commit_message%
-    if not "%commit_message%"=="" (
-        echo %commit_message%
+    pause
+    if not "!commit_message!"=="" (
         pause
         cls
-        echo %title%
+        echo !title!
         echo.
-        git commit -m "%commit_message%"
+        git commit -m "!commit_message!"
         git remote remove origin
         git remote add origin https://github.com/xspo-oky/iris-test
         git fetch
@@ -28,31 +28,29 @@ if "%pushyn%"=="Y" (
         echo.
         echo Which branch do you want to push to?
         set /p branch=
-        git push origin "%branch%"
+        git push origin "!branch!"
         timeout /t 5
         cls
-        echo %title%
+        echo !title!
         python index.py
     ) else (
         cls
-        echo %title%
+        echo !title!
         echo.
         echo Invalid commit message input
         timeout /t 5
+        goto:end
       )
 ) else (
-    if "%pushyn%"=="y" (
+    if "!pushyn!"=="y" (
     git add .
     echo Input Commit Message:
     set /p commit_message=
-    echo %commit_message%
-    if not "%commit_message%"=="" (
-        echo %commit_message%
-        pause
+    if not "!commit_message!"=="" (
         cls
-        echo %title%
+        echo !title!
         echo.
-        git commit -m "%commit_message%"
+        git commit -m "!commit_message!"
         git remote remove origin
         git remote add origin https://github.com/xspo-oky/iris-test
         git fetch
@@ -61,25 +59,27 @@ if "%pushyn%"=="Y" (
         echo.
         echo Which branch do you want to push to?
         set /p branch=
-        git push origin "%branch%"
+        git push origin "!branch!"
         timeout /t 5
         cls
-        echo %title%
+        echo !title!
         python index.py
     ) else (
         cls
-        echo %title%
+        echo !title!
         echo.
         echo Invalid commit message input
         timeout /t 5
+        goto:end
       )
     ) else (
-        if "%pushyn%"=="n" (
+        if "!pushyn!"=="n" (
             cls
-            echo %title%
+            echo !title!
             python index.py
         ) else (
             echo This is not a valid input.
+            pause
             goto:end
           )
       )
